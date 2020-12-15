1104a54b780f2d83839d9729c8d251fc12cc4f45
Eliyahu <eliyahumizrahi123@gmail.com>
2020-12-14 22:58:36 -0500

    assorted updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //THIS IS THE ONLY TIME we do direct assignment
+    //to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //We called setState!!
+        this.setState({ lat: position.coords.latitude });
+      },
+
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidMount() {
+    console.log('My component was just rendered on to the screen');
+  }
+
+  componentDidUpdate() {
+    console.log('Component was just updated');
+  }
+
+  //React says we have to define render
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
